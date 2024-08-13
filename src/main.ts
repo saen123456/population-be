f6efa6db16feb87e59e9c7776a4444735f695393
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-08-13 17:26:47 +0700

    fix: railway

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors();
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
